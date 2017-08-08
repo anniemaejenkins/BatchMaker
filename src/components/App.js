9a03e60bbfd4f9562dfbeb0e6c1f48e26e1e129b
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-08-08 11:54:27 -0400

    added custom methods to sign up form

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './../styles/App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this._handleUsername = this._handleUsername.bind(this);
+    this._handlePassword = this._handlePassword.bind(this);
+  }
+  _handleUsername(event) {
+    this.setState({
+      username: event.target.value
+    });
+  }
+  _handlePassword(event) {
+    this.setState({
+      password: event.target.value
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -11,11 +30,11 @@ class App extends Component {
           <form>
             <div>
               <label>Username</label>
-              <input type="text" />
+              <input type="text" value={ this.state.username } onChange={ this._handleUsername }/>
             </div>
             <div>
               <label>Password</label>
-              <input type="password" />
+              <input type="password" value={ this.state.password } onChange={ this._handlePassword }/>
             </div>
           </form>
         </div>
