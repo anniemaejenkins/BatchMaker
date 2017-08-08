e46c23ec777f5f3f8542ca0a3f5a5a0bd519fd66
Annie-mae Jenkins <annie-maejenkins@Annie-maes-MacBook-Pro.local>
2017-08-08 11:44:18 -0400

    added login and sign up forms on home page

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      hey
+        <h1>Welcome to Batch Maker, a recipe making site!</h1>
+        <div className="signup">
+          <h2>Sign Up!</h2>
+          <form>
+            <div>
+              <label>Username</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="password" />
+            </div>
+          </form>
+        </div>
+        <div className="login">
+          <h2>Log In!</h2>
+          <form>
+            <div>
+              <label>Username</label>
+              <input type="text" />
+            </div>
+            <div>
+              <label>Password</label>
+              <input type="password" />
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
